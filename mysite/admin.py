7530262ad9a8a6a4c5c400afdaf8c25262a3b4cd
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2023-08-08 23:04:23 +0900

    4주차

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MainContent
-admin.site.register(MainContent)
+from .models import MainContent,Comment
+
+class MainContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'pub_date']
+    search_fields = ['title']
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content_list', 'content', 'author', 'create_date', 'modify_date']
+    search_fields = ['author']
+
+admin.site.register(MainContent, MainContentAdmin)
+admin.site.register(Comment, CommentAdmin)
